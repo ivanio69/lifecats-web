d8561a2cea94804779da1c143720bcbd9ba653a6
lifecats <lifecatsup@mail.ru>
2020-12-19 15:45:53 +0000

    :))

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import React from 'react';
+import React from "react";
 import {
   GithubOutlined,
   YoutubeFilled,
   TwitterOutlined,
+  InstagramOutlined,
   GoogleOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 
 function App() {
   return (
     <div className="App">
       <div class="wrapper">
-        <div class="letters"><span class="letter">L</span><span class="letter">I</span><span class="letter">F</span><span class="letter">E</span><span class="letter">C</span><span class="letter">A</span><span class="letter">T</span><span class="letter">S</span></div>
+        <div class="letters">
+          <span class="letter">L</span>
+          <span class="letter">I</span>
+          <span class="letter">F</span>
+          <span class="letter">E</span>
+          <span class="letter">C</span>
+          <span class="letter">A</span>
+          <span class="letter">T</span>
+          <span class="letter">S</span>
+        </div>
         <p>Some random guy from internet</p>
-      </div><div className="box">
-
-        <a className="lnk" href="https://github.com/lifecats">
-          <GithubOutlined /> 
-        </a>
-        <a className="lnk" href="https://www.youtube.com/channel/UCj1RfokacfvbOAoV24CRiqg">
-          <YoutubeFilled /></a>
-        <a className="lnk" href="https://twitter.com/lifecats_of">
-          <TwitterOutlined /></a>
-        <a className="lnk" href="https://www.google.com/search?q=lifecats">
-          <GoogleOutlined /></a>
-
       </div>
-
-
+      <div className="box">
+        <a className="lnk" href="https://github.com/lifecats">
+          <GithubOutlined />
+        </a>
+        <a
+          className="lnk"
+          href="https://www.youtube.com/channel/UCj1RfokacfvbOAoV24CRiqg"
+        >
+          <YoutubeFilled />
+        </a>
+        <a className="lnk" href="https://twitter.com/lifecats_pog">
+          <TwitterOutlined />
+        </a>
+        <a className="lnk" href="https://www.google.com/search?q=lifecats">
+          <GoogleOutlined />
+        </a>
+        <a className="lnk" href="https://instagram.com/lifecats_of">
+          <InstagramOutlined />
+        </a>
+      </div>
     </div>
   );
 }
 
-export default App; 
+export default App;
