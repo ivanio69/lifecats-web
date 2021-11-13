1f4704d7eb3f6a76ebed0312792d878853a88a8f
/lifecats <lifecatsup@gmail.com>
2021-11-13 16:12:44 +0300

    First container

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,24 @@ export default function Main() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header className={styles.header}>
+        <h1>./lifecats</h1>
+        <a>Home</a>
+        <a>Link Shortener</a>
+        <div></div>
+      </header>
       <main>
-        <header className={styles.header}>
-          <h1>./lifecats</h1>
-          <a>Home</a>
-          <a>Link Shortener</a>
-          <div></div>
-        </header>
+        <div className={styles.container1}>
+          <div className={styles.titleBox}>
+            <h1 className={styles.titleBlack}>Web developer and musiciant</h1>
+            <h1 className={styles.titleBlue}>in one bundle.</h1>
+          </div>
+          <div className={styles.buttons}>
+            <button onClick={()=>{window.location.href="https://github.com/lifecats"}}>Check out my Github</button>
+            <br/>
+            <button onClick={()=>{window.location.href="https://open.spotify.com/artist/7M9VNbwTIQBVDIeamFHC0a?si=Dxjz4DoQTmyifFKWhkZ5mQ"}}>Listen to my music</button>
+          </div>
+        </div>
       </main>
     </div>
   );
