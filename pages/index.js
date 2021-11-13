e7668f17da3ac70c26b2b4e8cf9fb373797ec037
/lifecats <lifecatsup@gmail.com>
2021-11-13 22:50:10 +0300

    Second container: Recent projects

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Main.module.css";
+import Card from "./_card";
 
 export default function Main() {
   return (
@@ -17,7 +18,6 @@ export default function Main() {
         <h1>./lifecats</h1>
         <a>Home</a>
         <a>Link Shortener</a>
-        <div></div>
       </header>
       <main>
         <div className={styles.container1}>
@@ -26,10 +26,51 @@ export default function Main() {
             <h1 className={styles.titleBlue}>in one bundle.</h1>
           </div>
           <div className={styles.buttons}>
-            <button onClick={()=>{window.location.href="https://github.com/lifecats"}}>Check out my Github</button>
-            <br/>
-            <button onClick={()=>{window.location.href="https://open.spotify.com/artist/7M9VNbwTIQBVDIeamFHC0a?si=Dxjz4DoQTmyifFKWhkZ5mQ"}}>Listen to my music</button>
+            <button
+              onClick={() => {
+                window.location.href = "https://github.com/lifecats";
+              }}
+            >
+              Check out my Github
+            </button>
+            <br />
+            <button
+              onClick={() => {
+                window.location.href =
+                  "https://open.spotify.com/artist/7M9VNbwTIQBVDIeamFHC0a?si=Dxjz4DoQTmyifFKWhkZ5mQ";
+              }}
+            >
+              Listen to my music
+            </button>
           </div>
+        </div>
+
+        <div className={styles.container2}>
+          <h1>My lastest projects:</h1>
+          <ul>
+            <li>
+              <Card
+                title="lifecats.codes"
+                text="My personal website (this one)"
+                link="lifecats.codes"
+              />
+            </li>
+            <li>
+              <Card
+                title="S̴t̴r̶a̷n̵e̵"
+                text="A new geń̷͍̳̭̰̃͐̚e̸̖̦̣͔̯̓̽͊͑̈́r̶̢̛̺̖͕̯̦̯̤̿̆͂͑̂͑͑̎̀̉̚̕a̴͕͇̗͆̿͐͐̄̂̊̒͜t̸̝̞͍͎̰̜̤̰̲̞͓̞̥͗͛́̽̂̐͑į̴̨̛̟̼̯̱̳̹͌̊̎̂̉͑̽͋̚̕o̶͖̻̰̫̹̍n̶̢̢̏͂͌͘̚̚o̴͙̥̥̪̟̰̎͒̐́̿̍̆̿̈͘͘͝f̶͎̥͙͂͑̇̔̀̓͐̾̎̕-̵̜̟͚͎̪̯͉̘̓͛̀̐̈́̃̽̇͘͘̚-̸̥͇̗̣̬̄̎̌͠-̷̠͕͔̮̥͛̈́̈͝-̸̢͖͕͕̳̳̙̤͚̰̜͂̌-̶͓̺̽͗̏̎̔̾̒̔͒̒-̵̤̬̠͎͛̊́̈́̊̿́̕-̵̩͕̏̆̈́̇̓̋́͌̎̀̄́́͝ͅ-̶̱̩̅̀̀͋̄̿̂̏̎̓̕͠͝ͅ-̴̨̟̩̮̩̩̱̹̠̥̯̯̹͉͑̾͑̌̈́͒͒͋̕̕͝-̶̧͈͔̝̗̽̿͌̎̀̀͊̍̃̏̒́͘̕-̸̠̰̽̈́̑̈͒̿̅̅̅͊͐̕͝-̴̡̩̒̔͒̾̓̑̒̒̾̇̎̚-̷͕̜̤͙̳̬̬̔̕͝͠-̶̯͈̲͈̬̪̓́́̆̎͋̂̂̕̕̚͜-̸̺̺̼̙͈̣͇͈̼̙̪̥̩̲̄͂̽̓̑͛̽̆͝ͅ-̶̡̡̢̛͕̮̼̗̭͓̪̘̦͖͇͔̈́̓̽̂̅́̌̄̀̏͘͝͝-̷̢͈̦̙̜̺̩̙̩̜̙͓̟̾͛̌̀̄̈́̎̏͝-̶̡̨̢̫̻̻̻͈̹͔̬̒͛̔̿̎͘͠-̷̡̥̙͇̪͉̟̤̮̮̥̘̆̎-̸̖͓͕̊"
+                link="link: ḻ̶̛͌̅̑̍͑̄̒̑̓̈̚͠m̴̥͓̈́̉̔̈̄̇.̴̭̗̹͓̜̫̰̻̹̪̤̌̉̊̄͌͜͝e̷͓͙͗̌n̶̻̭̿͐̊̚ą̸̧̱̖̭̻̟̗͉̤̆͑̿̄̂́̏͝r̵̖͎̥͙͔͍̉͋̀t̴͎̠͊̂̉͐̆s̶̡̢̭̟̭̝̜̘̟̲̜͉̳̻̈́̇̃́̒͊̓͑̓͑́̕ͅ\̵̡̢̻̮̼̪̟̘̦̬̰̖̙̣͆̀͒͛̈͒̍́̇͝͝͠ͅ\̶̢̢̛̥̹̮̥͕̻͎̗̹̞̮̝̇̏̿͆̔̈́̓́̏͒̂̚:̷̧̡͍͔̱͙̼̝̮͎̭̦͕͒̓̎̿̊͂̊́̍̆͝ͅͅs̵̢̧̢͍͇̙͉̹̫̮̜̓p̶̧̰̃́̈́͐̓̒͋̉̃͒̇͛͘͘t̵̤̗͉͉̩̦͕̦̟̼͉̍͑̃̽̉̓̾̄̄͝t̸̰̭͇͓̟͉̭͈̦̖̼̩͂̏̓̈̓͂̕͜h̶̤̒̉̎̈́̌̊͐̾̐̈́̚͠"
+              />
+            </li>
+            <li>
+              <Card
+                title="Mydiary"
+                text="A simple way to store notes. 
+                Currently down, but available on github"
+                link="github.com/lifecats/mydiary"
+              />
+            </li>
+          </ul>
         </div>
       </main>
     </div>
