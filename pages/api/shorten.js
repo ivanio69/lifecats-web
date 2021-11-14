2421e4557d65dbc99aa02dcd706930157ebb3d21
/lifecats <lifecatsup@gmail.com>
2021-11-15 01:10:13 +0300

    recive and save links

---
+++ pages/api/shorten.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Link = mongoose.model("link", { link: String, slug: String });
+
+export default function handler(req, res) {
+  if (req.query.link !== undefined) {
+    console.log(req.query.link);
+    // connect mongo
+    mongoose.connect(process.env.MONGO_URI);
+
+    // generate slug
+    const generateSlug = () => {
+      let chars = "abcdefghijklmnopqrstuvwxyz";
+      let str = "";
+      for (let i = 0; i < 6; i++) {
+        str += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      return str;
+    };
+    const slug = generateSlug();
+
+    // Save to DB
+    const newLink = new Link({ link: req.query.link, slug });
+    newLink
+      .save()
+      .then(() => res.status(200).json({ link: req.query.link, slug }))
+      .catch((e) => {
+        console.error(e);
+        res.json({ error: e });
+      });
+  } else {
+    res.json({ error: "No `link` query." });
+  }
+}
