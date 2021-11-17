6ca19e74c03c0411034cf2884379930a3234f01f
/lifecats <lifecatsup@gmail.com>
2021-11-17 02:17:26 -0800

    First loading of page /lnk results slug=undefined

--- pages/lnk/[lnk].js
+++ pages/lnk/[lnk].js
@@ -3,7 +3,9 @@ import { useRouter } from 'next/router'
 const Post = () => {
   const router = useRouter()
   const { lnk } = router.query
-  if (process.browser)  
+  if (process.browser)
+  if (lnk !== undefined)  
+  console.log(lnk)
     window.location.replace('/api/recive?slug='+lnk)
   return true;
 }
