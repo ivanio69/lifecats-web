630622baef82924a2bdbb54925932f0ec8dbb116
/lifecats <lifecatsdev@gmail.com>
2022-02-22 13:17:21 -0800

    Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.background}>
+        <div className={styles.bgCircle1} />
+        <div className={styles.bgCircle2} />
+        <div className={styles.bgCircle3} />
+        <div className={styles.bgCircle4} />
+      </div>
+      <h1 className={styles.title}>lifecats</h1>
+      <ul className={styles.menu}>
+        <li>
+          <a href="/cloud">Cloud Storage</a>
+        </li>
+        <li>
+          <a href="/cloud">Link Shortener</a>
+        </li>
+        <li>
+          <a href="/cloud">LCINF</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
